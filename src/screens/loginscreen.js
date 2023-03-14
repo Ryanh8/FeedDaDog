138f0a4463849d1b4915d517f8f2856f7674e8ea
Ryanh8 <ryanhu20@gmail.com>
2023-03-14 14:11:49 -0400

    testing

---
+++ src/screens/loginscreen.js
@@ -0,0 +1,15 @@
+//create a home screen component
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const loginscreen = () => {
+  return (
+    <View>
+      <Text>loginscreen</Text>
+    </View>
+  )
+}
+
+export default loginscreen
+
+const styles = StyleSheet.create({})
